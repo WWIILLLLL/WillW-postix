a745bc60bbe483043601fd68540a618eb5445693
Givo <giv0online@gmail.com>
2024-04-08 20:49:46 -0500

    yes i fatfingered postfixc

--- CPP Postfixc/CPP Postfixc.cpp
+++ CPP Postfixc/CPP Postfixc.cpp
@@ -7,26 +7,50 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Enter a Postfix expression:" << endl;
+int DoPostfix(string expressionStart) {
+    bool founde = false;
+    int expressionLength = 0;
+    string expression = expressionStart;
 
-    string expression;
-    getline(cin, expression);
+    while (expression[expressionLength] != '=') {
+        for (int i = expressionLength; i < expression.size(); i++) {
+            if (expression[i] == '=') {
+                expressionLength = i;
+                founde = true;
+                break;
+            }
+        }
+
+        if (founde) break;
+
+        expressionLength = expression.size();
+        string tempExpression;
+        getline(cin, tempExpression);
+        expression += " " + tempExpression;
+    }
+
+    expression = expression.substr(0, expressionLength);
+
+    cout << expression << endl;
+
     stack<int> numberStack = stack<int>();
 
+    bool endEarly = false;
     for (int i = 0; i < expression.size(); i++) {
         while (expression[i] == ' ') {
             i++;
 
             if (i >= expression.size()) {
+                endEarly = true;
                 break;
             }
         }
 
+        if (endEarly) break;
+
         if (expression[i] == '+') {
             if (numberStack.size() < 2) {
-                cout << "Invalid postfix expression";
+                cout << "Invalid postfix expression" << endl;
                 return -1;
             }
 
@@ -41,7 +65,7 @@ int main()
 
         if (expression[i] == '*') {
             if (numberStack.size() < 2) {
-                cout << "Invalid postfix expression";
+                cout << "Invalid postfix expression" << endl;
                 return -1;
             }
 
@@ -55,22 +79,40 @@ int main()
         }
 
         int startChar = i;
-        
         while (expression[i] != ' ') {
             i++;
-
             if (i >= expression.size()) {
                 break;
             }
 
             if (expression[i] == '*' || expression[i] == '+') {
-                cout << "Invalid postfix expression";
+                cout << "Invalid postfix expression" << endl;
                 return -1;
             }
         }
 
-        numberStack.push(stoi(expression.substr(startChar, i)));
+        numberStack.push(stoi(expression.substr(startChar, i - startChar)));
     }
 
-    cout << "result: " << numberStack.top();
+    if (numberStack.size() > 1) {
+        cout << "Invalid postfix expression" << endl;
+        return -1;
+    }
+    else {
+        cout << "The result is: " << numberStack.top() << endl;
+    }
+}
+
+int main()
+{
+    cout << "Please type in a PostFix expressions (ending with =): " << endl;
+    string input;
+    getline(cin, input);
+
+    while (input[0] != 'q') {
+        int res = DoPostfix(input);
+
+        cout << "Please type in a PostFix expressions (ending with =): " << endl;
+        getline(cin, input);
+    }
 }
